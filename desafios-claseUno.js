f9b36d6b7edead1bb5ed841ff12e3c56e87455a4
John Forero <100742141+jonsdevsigned@users.noreply.github.com>
2024-07-06 23:54:58 -0500

    First commit. Programming logic challenge one

---
+++ desafios-claseUno.js
@@ -0,0 +1,43 @@
+//DESAFIO 1
+// 1. Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+// 2. Declara una variable llamada nombre y asígnale el valor "Luna".
+let nameUser = "Luna";
+
+console.log("Mi nombre es " + nameUser);
+
+//3. Crea una variable llamada edad y asígnale el valor 25.
+let age = 25;
+
+console.log("Tengo " + age + " años");
+
+//4. Establece una variable numeroDeVentas y asígnale el valor 50.
+let salesNumber = 50;
+
+console.log(salesNumber);
+
+//5. Establece una variable saldoDisponible y asígnale el valor 1000.
+let avialableBalance = 1000;
+
+console.log(avialableBalance);
+
+//6. Muestra una alerta con el texto "¡Error! Completa todos los campos".
+alert("¡Error! Completa todos los campos");
+
+//7. Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+let messageerror = "¡Error! Completa todos los campos";
+
+alert(messageerror);
+
+//8. Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre .
+nameUser = prompt("Cual es su nombre de usuario?");
+
+//9. Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+age = prompt("Ingrese su edad, por favor");
+
+//10. Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
+if (age >= 18) {
+  alert("!Puedes obtener tu licencia de conducir!");
+}
+
